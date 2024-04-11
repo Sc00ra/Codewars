c1d640366aaf3737694366770049739647c3a575
Sc00ra <scoorabartek@gmail.com>
2024-04-11 19:52:26 +0200

    day 7

---
+++ PowerShell/Even or Odd.ps1
@@ -0,0 +1,9 @@
+#https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+function Get-EvenOrOdd($number) {
+    if($number % 2 -eq 0){
+        return "Even"
+    }
+    else{
+        return "Odd"
+    }
+  }
---
+++ PowerShell/Grasshopper - Terminal game move function.ps1
@@ -0,0 +1,4 @@
+#https://www.codewars.com/kata/563a631f7cbbc236cf0000c2
+function TerminalMove([int] $pos, [int] $roll) {
+    return $pos + 2*$roll
+}
